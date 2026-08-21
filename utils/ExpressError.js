9d689a475518fe009f635a0b551663ec5975929d
Dipu Chauhan <184005124+Dipu-chauhan@users.noreply.github.com>
2026-08-21 14:27:52 +0530

    Delete utils directory

--- utils/ExpressError.js
+++
@@ -1,9 +0,0 @@
-class ExpreeError extends Error {
-    constructor(statusCode,message){
-        super();
-        this.statusCode = statusCode;
-        this.message = message;
-    }
-}
-
-module.exports = ExpreeError;
